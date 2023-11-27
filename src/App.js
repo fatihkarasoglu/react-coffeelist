594e61ed468c9ef964e27515eeb33b4f4fa4b118
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-11-27 19:07:42 +0300

    -- v.2

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import { useState } from "react";
-
 import CoffeeCards from "./components/CoffeeCards";
 
-// import CoffeeList from "./components/CoffeeList";
-// import Favorites from "./components/Favorites";
-
 function App() {
-
-  const [coffees, setCoffees] = useState([
-    { id: 1, name: 'Espresso', description: 'Strong and short coffee', image: 'espresso.jpg' },
-    // Add more coffee items
-  ]);
-  const [favorites, setFavorites] = useState([]);
-
-  // const addCoffeeToFavorites = coffee => {
-  //   setFavorites([...favorites, coffee]);
-  // };
-
-  // const voteForCoffee = coffeeId => {
-  //   // Implement voting logic
-  // };
 
   return (
     <div className="min-w-full min-h-screen bg-[url('./images/bg-cafe.jpg')] bg-no-repeat bg-contain bg-[#111315] flex justify-center items-center">
       <div className="flex justify-center items-center top-20">
         <CoffeeCards />
-        {/* <h1 className="text-4xl font-bold text-center my-8">Coffee Explorer</h1>
-        <CoffeeList
-          coffees={coffees}
-          onAddToFavorites={addCoffeeToFavorites}
-          onVote={voteForCoffee}
-        />
-        <h2 className="text-2xl font-bold text-center my-8">Favorites</h2>
-        <Favorites favorites={favorites} /> */}
       </div>
     </div>
   );
--- src/components/CoffeeCards.js
+++ src/components/CoffeeCards.js
@@ -1,3 +1,5 @@
+import { Rating } from '@mui/material';
+
 const espresso = require('../images/espresso.jpg');
 const cappuccino = require('../images/cappuccino.jpg');
 const turkishCoffee = require('../images/turkishcoffee.jpg');
@@ -14,42 +16,58 @@ export default function CoffeeCards() {
                 <h2 className='text-s font-display text-center'>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</h2>
             </div>
             <div className='w-3/4 mx-10 my-24 justify-center items-center'>
-            <ul className=" flex flex-row justify-evenly items-center">
-                <li>
-                    <img alt="" src={espresso} className="w-60 h-48 rounded-xl" />
-                    <div className='flex justify-between'>
-                        <span>Espresso</span>
-                        <span className="px-1 rounded bg-amber-400 text-black">100₺</span>
-                    </div>
-                </li>
-                <li>
-                    <img alt="" src={cappuccino} className="w-60 h-48 rounded-xl" />
-                    <p>Cappuccino</p>
-                    <p>75₺</p>
-                </li>
-                <li>
-                    <img alt="" src={turkishCoffee} className="w-60 h-48 rounded-xl" />
-                    <p>TÜRK Kahvesi</p>
-                    <p>125₺</p>
-                </li>
-            </ul>
-            <ul className="w-full flex flex-row justify-evenly items-center mt-14 pb-14">
-                <li>
+                <ul className=" flex flex-row justify-evenly items-center">
+                    <li>
+                        <img alt="" src={espresso} className="w-60 h-48 rounded-xl" />
+                        <div className='flex justify-between mt-2'>
+                            <span>Espresso</span>
+                            <span className="px-1 rounded bg-cyan-200 font-bold text-black">100₺</span>
+                        </div>
+                        <Rating name="half-rating" size='small' defaultValue={4} precision={0.5} />
+                    </li>
+                    <li>
+                        <img alt="" src={cappuccino} className="w-60 h-48 rounded-xl" />
+                        <div className='flex justify-between mt-2'>
+                            <span>Cappuccino</span>
+                            <span className="px-1 rounded bg-cyan-200 font-bold text-black">90₺</span>
+                        </div>
+                        <Rating name="half-rating" size='small' defaultValue={3.5} precision={0.5} />
+                    </li>
+                    <li>
+                        <img alt="" src={turkishCoffee} className="w-60 h-48 rounded-xl" />
+                        <div className='flex justify-between mt-2'>
+                            <span>TÜRK Kahvesi</span>
+                            <span className="px-1 rounded bg-cyan-200 font-bold text-black">125₺</span>
+                        </div>
+                        <Rating name="half-rating" size='small' defaultValue={4.5} precision={0.5} />
+                    </li>
+                </ul>
+                <ul className="w-full flex flex-row justify-evenly items-center mt-14 pb-14">
+                    <li>
                         <img alt="" src={coldcoffee} className="w-60 h-48 rounded-xl" />
-                        <p>Cold Coffee</p>
-                        <p>100₺</p>
+                        <div className='flex justify-between mt-2'>
+                            <span>Soğuk Kahve</span>
+                            <span className="px-1 rounded bg-cyan-200 font-bold text-black">95₺</span>
+                        </div>
+                        <Rating name="half-rating" size='small' defaultValue={2.5} precision={0.5} />
                     </li>
                     <li>
                         <img alt="" src={mocha} className="w-60 h-48 rounded-xl" />
-                        <p>Mocha</p>
-                        <p>75₺</p>
+                        <div className='flex justify-between mt-2'>
+                            <span>Mocha</span>
+                            <span className="px-1 rounded bg-cyan-200 font-bold text-black">70₺</span>
+                        </div>
+                        <Rating name="half-rating" size='small' defaultValue={4} precision={0.5} />
                     </li>
                     <li>
                         <img alt="" src={latte} className="w-60 h-48 rounded-xl" />
-                        <p>Latte</p>
-                        <p>125₺</p>
+                        <div className='flex justify-between mt-2'>
+                            <span>Latte</span>
+                            <span className="px-1 rounded bg-cyan-200 font-bold text-black">65₺</span>
+                        </div>
+                        <Rating name="half-rating" size='small' defaultValue={3.5} precision={0.5} />
                     </li>
-            </ul>
+                </ul>
             </div>
         </div>
     )
